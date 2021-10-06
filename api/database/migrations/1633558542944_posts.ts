9513ec3548b98cc6333e34d0ee8913a0886acde3
Ali Mert Çakar <mail@alimert.net>
2021-10-07 01:22:29 +0300

    make migration

---
+++ api/database/migrations/1633558542944_posts.ts
@@ -0,0 +1,20 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Posts extends BaseSchema {
+  protected tableName = 'posts'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('username')
+      table.string('password')
+      table.string('fullname')
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
