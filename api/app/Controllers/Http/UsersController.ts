3a82b100343e336084c132aff73fe943fee69774
Ali Mert Çakar <mail@alimert.net>
2021-10-07 23:35:42 +0300

    code

--- api/app/Controllers/Http/UsersController.ts
+++ api/app/Controllers/Http/UsersController.ts
@@ -1,16 +1,16 @@
 // import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import User from 'App/Models/User';
-import Database from '@ioc:Adonis/Lucid/Database';
+import User from "App/Models/User";
+import Database from "@ioc:Adonis/Lucid/Database";
 
 export default class UsersController {
   public async index() {
     // const userList = await User.all();
-    return { selam: 'userList!' };
+    return { selam: "userList!!!" };
   }
 
   public async addUser() {
     const user = new User();
-    user.fullname = 'Ali Mert Çakar';
+    user.fullname = "Ali Mert Çakar";
     await user.save();
     return { ok: true };
   }
