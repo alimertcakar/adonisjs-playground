8c2d0e7b8bb2620e11727a8da277d81a1a0b3ec2
Ali Mert Çakar <mail@alimert.net>
2021-10-09 13:57:40 +0300

    code

--- api/database/seeders/User.ts
+++ api/database/seeders/User.ts
@@ -1,7 +1,14 @@
-import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder';
+import User from 'App/Models/User';
 
 export default class UserSeeder extends BaseSeeder {
-  public async run () {
-    // Write your database queries inside the run method
+  public async run() {
+    const uniqueKey = 'user seed';
+
+    await User.updateOrCreate(uniqueKey, {
+      fullname: 'Ali Mert Çakar',
+      username: 'alimert',
+      password: '1234',
+    });
   }
 }
