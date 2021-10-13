30fd03733853abc749ff251847ae952e410540d6
Ali Mert Çakar <mail@alimert.net>
2021-10-13 22:51:56 +0300

    .

--- api/database/migrations/1633558542944_user_account.ts
+++ api/database/migrations/1633558542944_user_account.ts
@@ -1,20 +1,20 @@
-import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+import BaseSchema from '@ioc:Adonis/Lucid/Schema';
 
 export default class UserAccount extends BaseSchema {
-  protected tableName = 'user_account'
+  protected tableName = 'user_account';
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
-      table.string('username')
-      table.string('password')
-      table.string('fullname')
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
-    })
+      table.increments('id');
+      table.string('username');
+      table.string('password');
+      table.string('fullname');
+      table.timestamp('created_at', { useTz: true });
+      table.timestamp('updated_at', { useTz: true });
+    });
   }
 
   public async down() {
-    this.schema.dropTable(this.tableName)
+    this.schema.dropTable(this.tableName);
   }
 }
